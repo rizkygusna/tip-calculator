e40e9c4563022d3c8eb9445595084a60347947d3
rizkygusna <gusnafarid@gmail.com>
2021-12-15 18:49:22 +0700

    add style to element states

---
+++ index.js
@@ -0,0 +1,8 @@
+const bill = document.querySelector('#bill');
+const people = document.querySelector('#people');
+const customTip = document.querySelector('#customTip');
+
+const tipAmount = document.querySelector('.amount-number');
+const total = document.querySelector('.total-number');
+
+console.log(bill.value, tipAmount.innerHTML);
